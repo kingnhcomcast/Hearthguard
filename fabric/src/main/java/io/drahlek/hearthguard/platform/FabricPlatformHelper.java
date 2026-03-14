32dd945b5f92bc03aa225b4f6a27a8d3825d2e57
Kevin King <kingnh@comcast.net>
2026-03-13 21:30:24 -0400

    package, mod id, name, etc. rename add modmenu to fabric

---
+++ fabric/src/main/java/io/drahlek/hearthguard/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package io.drahlek.hearthguard.platform;
+
+import io.drahlek.hearthguard.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
