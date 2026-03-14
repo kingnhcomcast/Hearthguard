8972e8f22809c2f03b4df11f2cd439e1c448dee0
Kevin King <kingnh@comcast.net>
2026-03-13 20:34:25 -0400

    initial multiloader, all loaders run

--- src/main/java/io/drahlek/hearthguard/entity/FearDropTracker.java
+++
@@ -1,7 +0,0 @@
-package io.drahlek.hearthguard.entity;
-
-public interface FearDropTracker {
-    boolean hearthguard$hasDroppedFearItem();
-
-    void hearthguard$setDroppedFearItem(boolean droppedFearItem);
-}
